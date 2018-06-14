00f1d52b423fb7c5d9d84446bf4af5d1870f6880
John Moranski <johnnymoranski21@gmail.com>
2018-06-14 12:54:52 -0500

    first commit

---
+++ src/components/Welcome.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import '../css/Welcome.css';
+import NavbarFeatures from './Navigation.js';
+
+
+
+class Welcome extends Component {
+	constructor(){
+		super();
+
+	};
+
+
+render(){
+	return(
+		<h1>Hello</h1>
+				);
+	}
+}
+
+export default Welcome;
